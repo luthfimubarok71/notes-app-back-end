9b190ccbd8a1d1518d66f86afd1ceb595a6dd63b
Luthfi <luthfimubarok71@gmail.com>
2025-02-01 14:38:01 +0700

    add logic for putNoteByIdHandler

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -59,8 +59,24 @@ class NotesHandler {
     }
   }
 
-  putNoteByIdHandler() {
+  putNoteByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
 
+      this._service.editNoteById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Catatan berhasil diperbarui',
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(404);
+      return response;
+    }
   }
 
   deleteNoteByIdHandler() {
