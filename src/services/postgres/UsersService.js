5bc8dfd3706a335298425e6eb677f7e53310c3b1
Luthfi <luthfimubarok71@gmail.com>
2025-03-06 14:50:24 +0700

    create a users table with the migrate technique and also create usersService

---
+++ src/services/postgres/UsersService.js
@@ -0,0 +1,45 @@
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+
+class UserService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    // verifikasi Username
+    await this.verifyNewUsername(username);
+
+    const id = nanoid(16);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT INTO users VALUES ($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT * FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
+    }
+  }
+}
+
+module.exports = UserService;
